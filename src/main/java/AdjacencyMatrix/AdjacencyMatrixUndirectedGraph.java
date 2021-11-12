d011c6d98fe871a3ba8a2fe0c7bc634ea210d595
Simon SASSI <12765875+maxerbox@users.noreply.github.com>
2021-11-12 22:22:57 +0100

    feat: final implem

--- src/main/java/AdjacencyMatrix/AdjacencyMatrixUndirectedGraph.java
+++ src/main/java/AdjacencyMatrix/AdjacencyMatrixUndirectedGraph.java
@@ -15,130 +15,136 @@ import java.util.List;
  * It is possible to have simple and multiple graph
  */
 public class AdjacencyMatrixUndirectedGraph extends AbstractMatrixGraph<UndirectedNode> implements IUndirectedGraph {
-	
-	//--------------------------------------------------
-	// 				Constructors
-	//-------------------------------------------------- 
-	
-	public AdjacencyMatrixUndirectedGraph() {
-		super();
-	}
-	
-	public AdjacencyMatrixUndirectedGraph(int[][] mat) {
-		this.order=mat.length;
-		this.matrix = new int[this.order][this.order];
-		for(int i = 0; i<this.order; i++){
-			for(int j = i; j<this.order; j++){
-				this.matrix[i][j] = mat[i][j];
-				this.matrix[j][i] = mat[i][j];
-				this.m += mat[i][j];
-			}
-		}	
-	}
-	
-	public AdjacencyMatrixUndirectedGraph(IUndirectedGraph g) {
-		this.order = g.getNbNodes(); 				
-		this.m = g.getNbEdges(); 				
-		this.matrix = g.toAdjacencyMatrix(); 
-	}
 
-	//--------------------------------------------------
-	// 					Accessors
-	//--------------------------------------------------
+    //--------------------------------------------------
+    // 				Constructors
+    //--------------------------------------------------
 
-	@Override
-	public int getNbEdges() {
-		return this.m;
-	}
+    public AdjacencyMatrixUndirectedGraph() {
+        super();
+    }
 
-	public List<Integer> getNeighbours(AbstractNode x) {
-		List<Integer> l = new ArrayList<>();
-		for(int i = 0; i<matrix[x.getLabel()].length; i++){
-			if(matrix[x.getLabel()][i]>0){
-				l.add(i);
-			}
-		}
-		return l;
-	}
-	
-	// ------------------------------------------------
-	// 					Methods 
-	// ------------------------------------------------		
+    public AdjacencyMatrixUndirectedGraph(int[][] mat) {
+        this.order = mat.length;
+        this.matrix = new int[this.order][this.order];
+        for (int i = 0; i < this.order; i++) {
+            for (int j = i; j < this.order; j++) {
+                this.matrix[i][j] = mat[i][j];
+                this.matrix[j][i] = mat[i][j];
+                this.m += mat[i][j];
+            }
+        }
+    }
 
-	/**
-	 * Edge = arête
-	 * >= 1 car on est en multi graph (on peut avoir plusieurs arêtes entre deux noeuds
-	 */
-	@Override
-	public boolean isEdge(UndirectedNode x, UndirectedNode y) {
-		return this.matrix[x.getLabel()][y.getLabel()] >= 1 && this.matrix[y.getLabel()][x.getLabel()] >= 1;
-	}
-	
-	/**
+    public AdjacencyMatrixUndirectedGraph(IUndirectedGraph g) {
+        this.order = g.getNbNodes();
+        this.m = g.getNbEdges();
+        this.matrix = g.toAdjacencyMatrix();
+    }
+
+    //--------------------------------------------------
+    // 					Accessors
+    //--------------------------------------------------
+
+    @Override
+    public int getNbEdges() {
+        return this.m;
+    }
+
+    public List<Integer> getNeighbours(AbstractNode x) {
+        List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < matrix[x.getLabel()].length; i++) {
+            if (matrix[x.getLabel()][i] > 0) {
+                l.add(i);
+            }
+        }
+        return l;
+    }
+
+    // ------------------------------------------------
+    // 					Methods
+    // ------------------------------------------------
+
+    /**
+     * Edge = arête
+     * >= 1 car on est en multi graph (on peut avoir plusieurs arêtes entre deux noeuds
+     */
+    @Override
+    public boolean isEdge(UndirectedNode x, UndirectedNode y) {
+        return this.matrix[x.getLabel()][y.getLabel()] > 0 && this.matrix[y.getLabel()][x.getLabel()] > 0;
+    }
+
+    /**
      * removes the edge (x,y) if there exists at least one between these nodes in the graph.
      */
-	@Override
-	public void removeEdge(UndirectedNode x, UndirectedNode y) {
-		if(isEdge(x,y)) {
-			this.matrix[x.getLabel()][y.getLabel()]--;
-			this.matrix[y.getLabel()][x.getLabel()]--;
-		}
-	}
+    @Override
+    public void removeEdge(UndirectedNode x, UndirectedNode y) {
+        if (isEdge(x, y)) {
+            this.matrix[x.getLabel()][y.getLabel()]--;
+            this.matrix[y.getLabel()][x.getLabel()]--;
+        }
+    }
 
-	/**
+    /**
      * adds the edge (x,y), we allow the multi-graph.
      */
-	@Override
-	public void addEdge(UndirectedNode x, UndirectedNode y) {
-		this.matrix[x.getLabel()][y.getLabel()]++;
-		this.matrix[y.getLabel()][x.getLabel()]++;
-	}
+    @Override
+    public void addEdge(UndirectedNode x, UndirectedNode y) {
+        this.matrix[x.getLabel()][y.getLabel()]++;
+        this.matrix[y.getLabel()][x.getLabel()]++;
+    }
 
-	
-	/**
+
+    /**
      * @return the adjacency matrix representation int[][] of the graph
      */
-	public int[][] toAdjacencyMatrix() {
-		return this.matrix;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder s = new StringBuilder("Adjacency Matrix: \n");
-		for (int[] ints : this.matrix) {
-			for (int anInt : ints) {
-				s.append(anInt).append(" ");
-			}
-			s.append("\n");
-		}
-		s.append("\n");
-		return s.toString();
-	}
+    public int[][] toAdjacencyMatrix() {
+        return this.matrix;
+    }
 
-	public static void main(String[] args) {
-		int[][] mat2 = GraphTools.generateGraphData(10, 35, false, true, false, 100001);
-		GraphTools.afficherMatrix(mat2);
-		AdjacencyMatrixUndirectedGraph am = new AdjacencyMatrixUndirectedGraph(mat2);
-		System.out.println(am);
-		System.out.println("N = "+am.getNbNodes()+ "\n M = "+am.getNbEdges());
-		List<Integer> t2 = am.getNeighbours(new UndirectedNode(2));
-		for (Integer integer : t2) {
-			System.out.print(integer + ", ");
-		}
-		am.isEdge(new UndirectedNode(2), new UndirectedNode(5));
-		for(int i = 0; i<3;i++)
-			am.addEdge(new UndirectedNode(2), new UndirectedNode(5));
-		System.out.println(am);
-		am.removeEdge(new UndirectedNode(2), new UndirectedNode(5));
-		System.out.println(am);
-		// A completer
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder("Adjacency Matrix: \n");
+        for (int[] ints : this.matrix) {
+            for (int anInt : ints) {
+                s.append(anInt).append(" ");
+            }
+            s.append("\n");
+        }
+        s.append("\n");
+        return s.toString();
+    }
 
-		System.out.println("test isEdge true = " + am.isEdge(new UndirectedNode(2), new UndirectedNode(5)));
-		System.out.println("test isEdge false = " + am.isEdge(new UndirectedNode(0), new UndirectedNode(0)));
-		am.addEdge(new UndirectedNode(0), new UndirectedNode(0));
-		System.out.println("test isEdge true = " + am.isEdge(new UndirectedNode(0), new UndirectedNode(0)));
-		am.removeEdge(new UndirectedNode(0), new UndirectedNode(0));
-		System.out.println("test removeEdge ok si false = " + am.isEdge(new UndirectedNode(0), new UndirectedNode(0)));
+    public static void main(String[] args) {
+        int[][] mat2 = GraphTools.generateGraphData(10, 35, false, true, false, 100001);
+        GraphTools.afficherMatrix(mat2);
+        AdjacencyMatrixUndirectedGraph am = new AdjacencyMatrixUndirectedGraph(mat2);
+        System.out.println(am);
+        System.out.println("N = " + am.getNbNodes() + "\n M = " + am.getNbEdges());
+        List<Integer> t2 = am.getNeighbours(new UndirectedNode(2));
+        for (Integer integer : t2) {
+            System.out.print(integer + ", ");
+        }
+        am.isEdge(new UndirectedNode(2), new UndirectedNode(5));
+        for (int i = 0; i < 3; i++)
+            am.addEdge(new UndirectedNode(2), new UndirectedNode(5));
+        System.out.println(am);
+        am.removeEdge(new UndirectedNode(2), new UndirectedNode(5));
+        System.out.println(am);
+        // A completer
 
-	}
+        System.out.println("test isEdge true = " + am.isEdge(new UndirectedNode(2), new UndirectedNode(5)));
+        assert am.isEdge(new UndirectedNode(2), new UndirectedNode(5));
+        for (int i = 0; i < 5; i++) {
+            am.removeEdge(new UndirectedNode(2), new UndirectedNode(5));
+        }
+
+        assert !am.isEdge(new UndirectedNode(2), new UndirectedNode(5));
+        System.out.println("test isEdge false = " + am.isEdge(new UndirectedNode(0), new UndirectedNode(0)));
+        am.addEdge(new UndirectedNode(0), new UndirectedNode(0));
+        System.out.println("test isEdge true = " + am.isEdge(new UndirectedNode(0), new UndirectedNode(0)));
+        am.removeEdge(new UndirectedNode(0), new UndirectedNode(0));
+        System.out.println("test removeEdge ok si false = " + am.isEdge(new UndirectedNode(0), new UndirectedNode(0)));
+
+    }
 }
--- src/main/java/GraphAlgorithms/BinaryHeap.java
+++ src/main/java/GraphAlgorithms/BinaryHeap.java
@@ -28,35 +28,47 @@ public class BinaryHeap {
 
     //On ne swap pas si la valeur du parent = element
     public void insert(int element) {
-    	this.nodes.add(element);
+        /*    if (nodes.size() == 0) {
+            nodes.add(element);
+        } else {
+            nodes.add(element);
+            int src = (nodes.size() - 1) / 2;
+            int dest = nodes.size();
+            while (nodes.get(src) >= nodes.get(dest) && dest > 0) {
+                swap(src, dest);
+                dest = src;
+                src = (src - 1) / 2;
+            }
+        }*/
+        this.nodes.add(element);
 
         int posElement = nodes.lastIndexOf(element);
-    	int posFather = (posElement - 1)/2;
+        int posFather = (posElement - 1) / 2;
 
-    	while(nodes.get(posElement) < nodes.get(posFather)) {
-    	    swap(posFather, posElement);
+        while (nodes.get(posElement) < nodes.get(posFather)) {
+            swap(posFather, posElement);
 
-    	    posElement = posFather;
-    	    posFather = (posElement - 1)/2;
+            posElement = posFather;
+            posFather = (posElement - 1) / 2;
         }
     }
 
     public int remove() {
-        if(nodes.size() <= 0) {
+        if (nodes.size() <= 0) {
             return -1;
-        } else if(nodes.size() == 1) {
+        } else if (nodes.size() == 1) {
             return nodes.remove(0);
         } else {
             //Echanger la racine avec la dernière feuille
-            swap(0, nodes.size()-1);
+            swap(0, nodes.size() - 1);
 
             //replacer la nouvelle racine
-            int valRoot = nodes.remove(nodes.size()-1);
+            int valRoot = nodes.remove(nodes.size() - 1);
             int posElement = 0; //position de l'élément à replacer
             int dest = getBestChildPos(posElement);
 
             System.out.println("Remove() ");
-            while(dest != -1 && dest < nodes.size() && nodes.get(posElement) >= nodes.get(dest)) {
+            while (dest != -1 && dest < nodes.size() && nodes.get(posElement) >= nodes.get(dest)) {
                 swap(posElement, dest);
                 posElement = dest;
                 dest = getBestChildPos(posElement);
@@ -68,6 +80,7 @@ public class BinaryHeap {
 
     /**
      * Retourne l'indice du meilleur des deux fils comparés entre eux (dans le cas ou fils2 existe)
+     *
      * @param src
      * @return
      */
@@ -75,52 +88,50 @@ public class BinaryHeap {
         if (isLeaf(src)) { // the leaf is a stopping case, then we return a default value
             return -1;
         } else {
-            if(2*src+2 >= nodes.size()) {
-                return 2*src+1;
+            if (2 * src + 2 >= nodes.size()) {
+                return 2 * src + 1;
             } else {
-                if(nodes.get(2*src+1) <= nodes.get(2*src+2)) {
-                    return 2*src+1;
+                if (nodes.get(2 * src + 1) <= nodes.get(2 * src + 2)) {
+                    return 2 * src + 1;
                 } else {
-                    return 2*src+2;
+                    return 2 * src + 2;
                 }
 
             }
         }
     }
 
-    
+
     /**
-	 * Test if the node is a leaf in the binary heap
-	 *
+     * Test if the node is a leaf in the binary heap
+     * <p>
      * Rq:Pas besoin de tester le second fils, si le 1er est une feuille, alors le second l'est
      *
-	 * @returns true if it's a leaf or false else
-	 * 
-	 */	
+     * @returns true if it's a leaf or false else
+     */
     private boolean isLeaf(int src) {
-    	return 2*src+1 >= nodes.size();
+        return 2 * src + 1 >= nodes.size();
     }
 
     private void swap(int father, int child) {
-        int temp = nodes.get(father);
-        nodes.set(father, nodes.get(child));
-        nodes.set(child, temp);
+        int temp = this.nodes.get(father);
+        this.nodes.set(father, nodes.get(child));
+        this.nodes.set(child, temp);
     }
 
     public String toString() {
         StringBuilder s = new StringBuilder();
-        for (int i = 0; i < nodes.size(); i++) {
+        for (int i = 0; i < this.nodes.size(); i++) {
             s.append(nodes.get(i)).append(", ");
         }
         return s.toString();
     }
 
     /**
-	 * Recursive test to check the validity of the binary heap
-	 * 
-	 * @returns a boolean equal to True if the binary tree is compact from left to right
-	 * 
-	 */
+     * Recursive test to check the validity of the binary heap
+     *
+     * @returns a boolean equal to True if the binary tree is compact from left to right
+     */
     public boolean test() {
         return this.isEmpty() || testRec(0);
     }
@@ -141,7 +152,7 @@ public class BinaryHeap {
 
     public static void main(String[] args) {
         BinaryHeap jarjarBin = new BinaryHeap();
-        System.out.println(jarjarBin.isEmpty()+"\n");
+        System.out.println(jarjarBin.isEmpty() + "\n");
         int k = 20;
         int m = k;
         int min = 2;
@@ -152,7 +163,7 @@ public class BinaryHeap {
             jarjarBin.insert(rand);
             k--;
         }
-     // A completer
+        // A completer
         System.out.println("\n" + jarjarBin);
         System.out.println(jarjarBin.test());
 
