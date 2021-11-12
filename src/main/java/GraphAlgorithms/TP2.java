612d41616bd80a7777bffb2710125621fccce619
geraud simon <geraud.simon99@gmail.com>
2021-11-12 23:19:19 +0100

    TP2 Algo parcours en largeur; NOT COMPLETE

--- src/main/java/GraphAlgorithms/TP2.java
+++ src/main/java/GraphAlgorithms/TP2.java
@@ -1,13 +1,19 @@
 package GraphAlgorithms;
 
+import AdjacencyList.DirectedGraph;
 import AdjacencyList.UndirectedGraph;
 import Nodes.UndirectedNode;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class TP2 {
+
+    // ------------------------------------------
+    // 	  Algorithme de parcours en profondeur
+    // ------------------------------------------
 
     /*Algorithme parcours_profondeur(G, s, visited)
         visited[v]=True
@@ -57,7 +63,7 @@ public class TP2 {
     /**
      * Algorithme du parcours en profondeur à partir de l'algorithme ExplorerGraphe()
      * @param g     graphe à étudier
-     * @return      ensemble des noeuds parcourus avec l'ordre conservé
+     * @return      ensemble des noeuds parcourus avec l'ordre de parcours conservé
      */
     public static Set<UndirectedNode> parcoursProfondeur2(UndirectedGraph g) {
         Set<UndirectedNode> visited = new HashSet<UndirectedNode>();
@@ -74,7 +80,7 @@ public class TP2 {
     /**
      * Algorithme du parcours en profondeur à partir de l'algorithme ExplorerGraphe()
      * @param s         sommet visité à cette itération
-     * @param visited   ensemble des noeuds parcourus avec l'ordre conservé
+     * @param visited   ensemble des noeuds parcourus avec l'ordre de parcours conservé
      */
     private static void visiterParcoursProfondeur2(UndirectedNode s, Set<UndirectedNode> visited) {
         visited.add(s);
@@ -82,9 +88,72 @@ public class TP2 {
         Map<UndirectedNode, Integer> neighbours = s.getNeighbours();
 
         for(Map.Entry<UndirectedNode, Integer> neighbour : neighbours.entrySet()) {
-            if(!visited.contains(neighbour))
+            if(!visited.contains(neighbour.getKey()))
                 visiterParcoursProfondeur2(neighbour.getKey(), visited);
         }
     }
 
+
+    // ------------------------------------------
+    // 	  Algorithme de parcours en largeur
+    // ------------------------------------------
+
+    /**
+     * Algorithme du parcours en largeur
+     * @param g     graphe à étudier
+     * @param s     noeud de départ
+     * @return      ensemble des noeuds parcourus avec l'ordre de parcours conservé
+     */
+    public static Set<UndirectedNode> parcoursLargeur(UndirectedGraph g, UndirectedNode s) {
+        //Initialiser le tableau de booleans indiquant si un noeud a été parcouru
+        boolean[] visited = new boolean[g.getNbNodes()];
+
+        for(int node = 0; node < visited.length; node ++)
+            visited[node] = false;
+        visited[s.getLabel()] = true;
+
+        //Conserver l'ordre de parcours des nodes
+        Set<UndirectedNode> orderNodes = new HashSet<>();
+
+        //FIFO
+        ArrayList<UndirectedNode> toVisit = new ArrayList<>();
+        toVisit.add(s);
+        orderNodes.add(s);
+
+        //Parcours du graphe
+        UndirectedNode sommetParcouru;
+        Map<UndirectedNode, Integer> neighbours;
+
+        while (!toVisit.isEmpty()) {
+            sommetParcouru = toVisit.get(0);
+            toVisit.remove(0);
+            orderNodes.add(sommetParcouru);
+
+            neighbours = sommetParcouru.getNeighbours();
+
+            for(Map.Entry<UndirectedNode, Integer> neighbour : neighbours.entrySet()) {
+                if(!visited[neighbour.getKey().getLabel()]) {
+                    visited[neighbour.getKey().getLabel()] = true;
+                    toVisit.add(neighbour.getKey());
+                }
+            }
+        }
+
+        return orderNodes;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] Matrix = GraphTools.generateGraphData(5, 10, false, false, true, 100001);
+        GraphTools.afficherMatrix(Matrix);
+        UndirectedGraph al = new UndirectedGraph(Matrix);
+        System.out.println(al);
+
+        System.out.println("Affichage parcoursEnProfondeur");
+        System.out.println(parcoursProfondeur2(al));
+
+        System.out.println("Affichage parcoursEnLargeur");
+        System.out.println(parcoursLargeur(al, al.getNodes().get(0)));
+
+    }
 }
