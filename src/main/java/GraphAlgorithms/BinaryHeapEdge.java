7acd9d34d92029bdeb0e491b648da7c1b928e144
Simon SASSI <12765875+maxerbox@users.noreply.github.com>
2021-11-12 23:20:15 +0100

    wip: prim algorithm with binary heap edge

--- src/main/java/GraphAlgorithms/BinaryHeapEdge.java
+++ src/main/java/GraphAlgorithms/BinaryHeapEdge.java
@@ -9,11 +9,11 @@ import Nodes.UndirectedNode;
 
 public class BinaryHeapEdge {
 
-	/**
-	 * A list structure for a faster management of the heap by indexing
-	 * 
-	 */
-	private  List<Triple<UndirectedNode,UndirectedNode,Integer>> binh;
+    /**
+     * A list structure for a faster management of the heap by indexing
+     * Complexité inchangée avec BinaryHeap.java
+     */
+    private List<Triple<UndirectedNode, UndirectedNode, Integer>> binh;
 
     public BinaryHeapEdge() {
         this.binh = new ArrayList<>();
@@ -24,165 +24,162 @@ public class BinaryHeapEdge {
     }
 
     /**
-	 * Insert a new edge in the binary heap
-	 * 
-	 * @param from one node of the edge
-	 * @param to one node of the edge
-	 * @param val the edge weight
-	 */
+     * Insert a new edge in the binary heap
+     *
+     * @param from one node of the edge
+     * @param to   one node of the edge
+     * @param val  the edge weight
+     */
     public void insert(UndirectedNode from, UndirectedNode to, int val) {
-    	if(isEmpty()) {
-			binh.add(new Triple<>(from, to, val));
-		} else {
-			binh.add(new Triple<>(from, to, val));
+        if (isEmpty()) {
+            binh.add(new Triple<>(from, to, val));
+        } else {
+            binh.add(new Triple<>(from, to, val));
 
-			int dest = binh.size() - 1;
-			int src = (dest -1)/2;
+            int dest = binh.size() - 1;
+            int src = (dest - 1) / 2;
 
-			while(dest > 0 && binh.get(src).getThird() > binh.get(dest).getThird()) {
-				swap(src, dest);
-				dest = src;
-				src = (src -1)/2;
-			}
-		}
+            while (dest > 0 && binh.get(src).getThird() > binh.get(dest).getThird()) {
+                swap(src, dest);
+                dest = src;
+                src = (src - 1) / 2;
+            }
+        }
     }
 
-    
+
     /**
-	 * Removes the root edge in the binary heap, and swap the edges to keep a valid binary heap
-	 * 
-	 * @return the edge with the minimal value (root of the binary heap)
-	 * 
-	 */
-    public Triple<UndirectedNode,UndirectedNode,Integer> remove() {
-    	if(binh.size() <= 0) {
-			return null;
-		} else if(binh.size() == 1) {
-    		return binh.remove(0);
-		} else {
-			//Echanger la racine avec la dernière feuille
-			swap(0, binh.size()-1);
+     * Removes the root edge in the binary heap, and swap the edges to keep a valid binary heap
+     *
+     * @return the edge with the minimal value (root of the binary heap)
+     */
+    public Triple<UndirectedNode, UndirectedNode, Integer> remove() {
+        if (binh.size() <= 0) {
+            return null;
+        } else if (binh.size() == 1) {
+            return binh.remove(0);
+        } else {
+            //Echanger la racine avec la dernière feuille
+            swap(0, binh.size() - 1);
 
-			//replacer la nouvelle racine
-			Triple<UndirectedNode, UndirectedNode, Integer> valRoot = binh.remove(binh.size()-1);
-			int posElement = 0; //position de l'élément à replacer
-			int dest = getBestChildPos(posElement);
+            //replacer la nouvelle racine
+            Triple<UndirectedNode, UndirectedNode, Integer> valRoot = binh.remove(binh.size() - 1);
+            int posElement = 0; //position de l'élément à replacer
+            int dest = getBestChildPos(posElement);
 
-			System.out.println("Remove() ");
-			while(dest != -1 && dest < binh.size() && binh.get(posElement).getThird() >= binh.get(dest).getThird()) {
-				swap(posElement, dest);
-				posElement = dest;
-				dest = getBestChildPos(posElement);
-			}
+            System.out.println("Remove() ");
+            while (dest != -1 && dest < binh.size() && binh.get(posElement).getThird() >= binh.get(dest).getThird()) {
+                swap(posElement, dest);
+                posElement = dest;
+                dest = getBestChildPos(posElement);
+            }
 
-			return valRoot;
-		}
+            return valRoot;
+        }
     }
-    
+
 
     /**
-	 * From an edge indexed by src, find the child having the least weight and return it
-	 * 
-	 * @param src an index of the list edges
-	 * @return the index of the child edge with the least weight
-	 */
+     * From an edge indexed by src, find the child having the least weight and return it
+     *
+     * @param src an index of the list edges
+     * @return the index of the child edge with the least weight
+     */
     private int getBestChildPos(int src) {
         if (isLeaf(src)) { // the leaf is a stopping case, then we return a default value
             return -1;
         } else {
-			if(2*src+2 >= binh.size()) {
-				return 2*src+1;
-			} else {
-				if(binh.get(2*src+1).getThird() <= binh.get(2*src+2).getThird()) {
-					return 2*src+1;
-				} else {
-					return 2*src+2;
-				}
+            if (2 * src + 2 >= binh.size()) {
+                return 2 * src + 1;
+            } else {
+                if (binh.get(2 * src + 1).getThird() <= binh.get(2 * src + 2).getThird()) {
+                    return 2 * src + 1;
+                } else {
+                    return 2 * src + 2;
+                }
 
-			}
+            }
         }
     }
 
     private boolean isLeaf(int src) {
-    	return 2*src+1 >= binh.size() - 1;
+        return 2 * src + 1 >= binh.size() - 1;
     }
 
-    
+
     /**
-	 * Swap two edges in the binary heap
-	 * 
-	 * @param father an index of the list edges
-	 * @param child an index of the list edges
-	 */
-    private void swap(int father, int child) {         
-    	Triple<UndirectedNode,UndirectedNode,Integer> temp = new Triple<>(binh.get(father).getFirst(), binh.get(father).getSecond(), binh.get(father).getThird());
-    	binh.get(father).setTriple(binh.get(child));
-    	binh.get(child).setTriple(temp);
+     * Swap two edges in the binary heap
+     *
+     * @param father an index of the list edges
+     * @param child  an index of the list edges
+     */
+    private void swap(int father, int child) {
+        Triple<UndirectedNode, UndirectedNode, Integer> temp = new Triple<>(binh.get(father).getFirst(), binh.get(father).getSecond(), binh.get(father).getThird());
+        binh.get(father).setTriple(binh.get(child));
+        binh.get(child).setTriple(temp);
     }
 
-    
+
     /**
-	 * Create the string of the visualisation of a binary heap
-	 * 
-	 * @return the string of the binary heap
-	 */
+     * Create the string of the visualisation of a binary heap
+     *
+     * @return the string of the binary heap
+     */
     public String toString() {
         StringBuilder s = new StringBuilder();
-        for (Triple<UndirectedNode,UndirectedNode,Integer> no: binh) {
+        for (Triple<UndirectedNode, UndirectedNode, Integer> no : binh) {
             s.append(no).append(", ");
         }
         return s.toString();
     }
-    
-    
-    private String space(int x) {
-		StringBuilder res = new StringBuilder();
-		for (int i=0; i<x; i++) {
-			res.append(" ");
-		}
-		return res.toString();
-	}
-	
-	/**
-	 * Print a nice visualisation of the binary heap as a hierarchy tree
-	 * 
-	 */	
-	public void lovelyPrinting(){
-		int nodeWidth = this.binh.get(0).toString().length();
-		int depth = 1+(int)(Math.log(this.binh.size())/Math.log(2));
-		int index=0;
-		
-		for(int h = 1; h<=depth; h++){
-			int left = ((int) (Math.pow(2, depth-h-1)))*nodeWidth - nodeWidth/2;
-			int between = ((int) (Math.pow(2, depth-h))-1)*nodeWidth;
-			int i =0;
-			System.out.print(space(left));
-			while(i<Math.pow(2, h-1) && index<binh.size()){
-				System.out.print(binh.get(index) + space(between));
-				index++;
-				i++;
-			}
-			System.out.println("");
-		}
-		System.out.println("");
-	}
-	
-	// ------------------------------------
-    // 					TEST
-	// ------------------------------------
 
-	/**
-	 * Recursive test to check the validity of the binary heap
-	 * 
-	 * @return a boolean equal to True if the binary tree is compact from left to right
-	 * 
-	 */
+
+    private String space(int x) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < x; i++) {
+            res.append(" ");
+        }
+        return res.toString();
+    }
+
+    /**
+     * Print a nice visualisation of the binary heap as a hierarchy tree
+     */
+    public void lovelyPrinting() {
+        int nodeWidth = this.binh.get(0).toString().length();
+        int depth = 1 + (int) (Math.log(this.binh.size()) / Math.log(2));
+        int index = 0;
+
+        for (int h = 1; h <= depth; h++) {
+            int left = ((int) (Math.pow(2, depth - h - 1))) * nodeWidth - nodeWidth / 2;
+            int between = ((int) (Math.pow(2, depth - h)) - 1) * nodeWidth;
+            int i = 0;
+            System.out.print(space(left));
+            while (i < Math.pow(2, h - 1) && index < binh.size()) {
+                System.out.print(binh.get(index) + space(between));
+                index++;
+                i++;
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+
+    // ------------------------------------
+    // 					TEST
+    // ------------------------------------
+
+    /**
+     * Recursive test to check the validity of the binary heap
+     *
+     * @return a boolean equal to True if the binary tree is compact from left to right
+     */
     private boolean test() {
         return this.isEmpty() || testRec(0);
     }
 
     private boolean testRec(int root) {
-    	int lastIndex = binh.size()-1; 
+        int lastIndex = binh.size() - 1;
         if (isLeaf(root)) {
             return true;
         } else {
@@ -192,21 +189,21 @@ public class BinaryHeapEdge {
                 return binh.get(left).getThird() >= binh.get(root).getThird() && testRec(left);
             } else {
                 return binh.get(left).getThird() >= binh.get(root).getThird() && testRec(left)
-                    && binh.get(right).getThird() >= binh.get(root).getThird() && testRec(right);
+                        && binh.get(right).getThird() >= binh.get(root).getThird() && testRec(right);
             }
         }
     }
 
     public static void main(String[] args) {
         BinaryHeapEdge jarjarBin = new BinaryHeapEdge();
-        System.out.println(jarjarBin.isEmpty()+"\n");
+        System.out.println(jarjarBin.isEmpty() + "\n");
         int k = 10;
         int m = k;
         int min = 2;
         int max = 20;
         while (k > 0) {
-            int rand = min + (int) (Math.random() * ((max - min) + 1));                        
-            jarjarBin.insert(new UndirectedNode(k), new UndirectedNode(k+30), rand);            
+            int rand = min + (int) (Math.random() * ((max - min) + 1));
+            jarjarBin.insert(new UndirectedNode(k), new UndirectedNode(k + 30), rand);
             k--;
         }
 
@@ -214,9 +211,9 @@ public class BinaryHeapEdge {
         jarjarBin.lovelyPrinting();
 
         jarjarBin.remove();
-		jarjarBin.lovelyPrinting();
-		jarjarBin.remove();
-		jarjarBin.lovelyPrinting();
+        jarjarBin.lovelyPrinting();
+        jarjarBin.remove();
+        jarjarBin.lovelyPrinting();
     }
 
 }
