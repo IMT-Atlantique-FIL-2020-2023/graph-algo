e8c316eb278973ee1538cddc215b02565d764269
geraud simon <geraud.simon99@gmail.com>
2021-11-16 19:20:21 +0100

    TP2 CFC partie 1

--- src/main/java/GraphAlgorithms/TP2.java
+++ src/main/java/GraphAlgorithms/TP2.java
@@ -7,6 +7,11 @@ import Nodes.UndirectedNode;
 import java.util.*;
 
 public class TP2 {
+    //Voir la partie CFC
+    private static int compt;
+    private static int[] debut;
+    private static int[] fin;
+
 
     // ------------------------------------------
     // 	  Algorithme de parcours en profondeur
@@ -63,8 +68,6 @@ public class TP2 {
      * @return      ensemble des noeuds parcourus avec l'ordre de parcours conservé
      */
     public static List<UndirectedNode> parcoursProfondeur2(UndirectedGraph g) {
-
-
         List<UndirectedNode> visited = new ArrayList<UndirectedNode>();
 
         for(UndirectedNode sommet : g.getNodes()) {
@@ -78,8 +81,9 @@ public class TP2 {
 
     /**
      * Algorithme du parcours en profondeur à partir de l'algorithme ExplorerGraphe()
-     * @param s         sommet visité à cette itération
-     * @param visited   ensemble des noeuds parcourus avec l'ordre de parcours conservé
+     * @param s             sommet visité à cette itération
+     * @param visited       ensemble des noeuds parcourus avec l'ordre de parcours conservé
+     * @param profondeur    étage actuel de l'arbre construit à partir du graphe
      */
     private static void visiterParcoursProfondeur2(UndirectedNode s, List<UndirectedNode> visited, int profondeur) {
         System.out.println("Profondeur : " + profondeur + ", noeud : " + s);
@@ -100,9 +104,6 @@ public class TP2 {
                     visiterParcoursProfondeur2(neighbour.getKey(), visited, profondeur + 1);
             } // Et je continue d'explorer mon arbre
         }
-
-
-
     }
 
 
@@ -155,17 +156,107 @@ public class TP2 {
     }
 
 
+    // ------------------------------------------------------------
+    // 	  Algorithme de calcul des composantes fortement connexe
+    // ------------------------------------------------------------
+
+    /**
+     * Algorithme du parcours en profondeur au cas des composantes fortement connexes
+     * @param g     graphe à étudier
+     * @return      ensemble des noeuds parcourus avec l'ordre de parcours conservé
+     */
+    public static List<UndirectedNode> parcoursProfondeurCFC(UndirectedGraph g) {
+        //Ordre de parcours des noeuds visités
+        List<UndirectedNode> visited = new ArrayList<UndirectedNode>();
+
+        //Classification dynamique du parcours du graphe
+        //0 = non parcouru, 1 = détection de ses voisins, 2 = exploration compléte
+        int[] visiteCFC = new int[g.getNbNodes()];
+
+        debut = new int[g.getNbNodes()];
+        fin = new int[g.getNbNodes()];
+        compt = 0;
+
+        for(int i = 0; i < g.getNbNodes(); i++) {
+            visiteCFC[i] = 0;
+            debut[i] = 0;
+            fin[i] = 0;
+        }
+
+
+        for(UndirectedNode sommet : g.getNodes()) {
+            if(!visited.contains(sommet)) {
+                visiterParcoursProfondeurCFC(sommet, visited, visiteCFC, 0);
+            }
+        }
+
+        //Affichage CFC
+        System.out.println("Afficher CFC liste noeuds");
+        for(int i = 0; i < g.getNbNodes(); i++) {
+            System.out.println("Etat: " + visiteCFC[i] + ", debut: " + debut[i] + ", fin: " + fin[i]);
+        }
+
+        return visited;
+    }
+
+    /**
+     * Algorithme du parcours en profondeur au cas des composantes fortement connexes
+     * @param s             sommet visité à cette itération
+     * @param visited       ensemble des noeuds parcourus avec l'ordre de parcours conservé
+     * @param visiteCFC     Classification dynamique du parcours du graphe
+     *                      0 = non parcouru, 1 = détection de ses voisins, 2 = exploration compléte
+     * @param profondeur    étage actuel de l'arbre construit à partir du graphe
+     */
+    private static void visiterParcoursProfondeurCFC(UndirectedNode s, List<UndirectedNode> visited, int[] visiteCFC, int profondeur) {
+        System.out.println("Visite CFC Profondeur : " + profondeur + ", noeud : " + s);
+        visited.add(s);
+
+        debut[s.getLabel()] = compt;
+        compt++;
+
+
+        visiteCFC[s.getLabel()] = 1;
+
+        /*Map<UndirectedNode, Integer> neighbours = s.getNeighbours();
+
+        for(Map.Entry<UndirectedNode, Integer> neighbour : neighbours.entrySet()) {
+            if(!visited.contains(neighbour.getKey()))
+                visiterParcoursProfondeur2(neighbour.getKey(), visited);
+        }*/
+
+        if( s.getNbNeigh() > 0 ) { // Si il n'y a plus de de fils disponible, Je remonte d'un cran
+            Map<UndirectedNode, Integer> neighbours = s.getNeighbours(); // Sinon je récupère la liste des fils
+
+            for(Map.Entry<UndirectedNode, Integer> neighbour : neighbours.entrySet()) {
+                if(!visited.contains(neighbour.getKey()))
+                    visiterParcoursProfondeurCFC(neighbour.getKey(), visited, visiteCFC, profondeur + 1);
+            } // Et on continue d'explorer l'arbre
+        }
+
+        visiteCFC[s.getLabel()] = 2;
+        fin[s.getLabel()] = compt;
+        compt++;
+
+    }
+
+
+
     public static void main(String[] args) {
         int[][] Matrix = GraphTools.generateGraphData(9, 20, false, false, true, 100001);
         GraphTools.afficherMatrix(Matrix);
         UndirectedGraph al = new UndirectedGraph(Matrix);
         System.out.println(al);
 
-        System.out.println("Affichage parcoursEnProfondeur");
+        //Question 20
+        System.out.println("Question 20 - Affichage parcoursEnProfondeur");
         System.out.println(parcoursProfondeur2(al));
 
-        System.out.println("Affichage parcoursEnLargeur");
+        //Question 21
+        System.out.println("Question 21 - Affichage parcoursEnLargeur");
         System.out.println(parcoursLargeur(al, al.getNodes().get(0)));
 
+        //Question 22 Composantes Fortement connexes
+        System.out.println("Question 22 - Affichage CFC");
+        System.out.println(parcoursProfondeurCFC(al));
     }
 }
